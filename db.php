71ca2b146a7c636fedc98ee4e1ffc45c438880ef
Danny Fareez bin Mohd Faridznadzri <danreez1307@gmail.com>
2025-12-16 15:54:34 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
-$servername = "sql202.infinityfree.com";
-$username = "if0_40685957"; // Default XAMPP username
-$password = "050713050039"; // Default XAMPP password
-$dbname = "if0_40685957_my_journey";
+$servername = "";
+$username = ""; // Default XAMPP username
+$password = ""; // Default XAMPP password
+$dbname = "";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
